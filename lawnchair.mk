3f2ba620d86f36979e304ce73824e9ae3160ec5a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-27 21:04:12 -0300

    lawnchair: override Pixel Launcher as well
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    commit 78039d8033e41a0cae9356897539aea1b9a47cd0
    Signed-off-by: Trickbot199 <mezackisilva@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,15 +1,9 @@
 # Lawnchair
-LAWN_ROOT := packages/apps/Lawnchair
-
 PRODUCT_SOONG_NAMESPACES += \
-    $(LAWN_ROOT)
+    vendor/lawnchair
 
 PRODUCT_PACKAGES += \
     Lawnchair \
-    Lawnicons \
-    Lawnfeed
-
-PRODUCT_PACKAGE_OVERLAYS += $(LAWN_ROOT)/overlay
-PRODUCT_COPY_FILES += \
-    $(LAWN_ROOT)/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lawnchair.xml \
-    $(LAWN_ROOT)/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+    LawnchairOverlay \
+    lawnchair-hiddenapi-package-whitelist \
+    privapp-permissions-lawnchair
