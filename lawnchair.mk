ed60a4b69d4a24fc90a3f102d38dd98b046494d6
Signed-off-by: Pranav <npv12@iitbbs.ac.in>
2021-11-27 21:10:45 -0300

    lawnicons: update to 20/11/2021
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    commit 49cf285c2a952d9868ac4bd9dd381577705ed8b0
    Signed-off-by: Trickbot199 <mezackisilva@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -2,8 +2,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/lawnchair
 
+PRODUCT_COPY_FILES += \
+    vendor/lawnchair/libs/libnrb.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libnrb.so
+
 PRODUCT_PACKAGES += \
     Lawnchair \
     LawnchairOverlay \
+    Lawnicons \
     lawnchair-hiddenapi-package-whitelist \
     privapp-permissions-lawnchair
